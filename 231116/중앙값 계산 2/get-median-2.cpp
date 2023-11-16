6553c48b8905f31d3804ca418f1df61451a767b8
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-11-16 20:16:39 +0900

    Title: 중앙값 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231116/중앙값 계산 2/get-median-2.cpp
+++ 231116/중앙값 계산 2/get-median-2.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main() {
-    // ios_base::sync_with_stdio(false);
-    // cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     
     int n;
     int input[101] = {0,};
     scanf("%d", &n);
 
     for(int i = 1; i <= n; i++){
-        scanf("%d", input+i);
+        scanf("%d", &input[i]);
         sort(input+1, input+i+1);
 
         if(i&1){
