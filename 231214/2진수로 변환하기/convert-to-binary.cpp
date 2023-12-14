ba4b947bb56891fd406374ca96a3fee2c6186c45
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-12-14 20:31:19 +0900

    Title: 2진수로 변환하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/2진수로 변환하기/convert-to-binary.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    string str;
+
+    cin >> n;
+
+    int cnt = 0;
+
+    while(n > 0){
+        str[cnt++] = n%2 + '0';
+        n /= 2;
+    }
+
+    for(int i = cnt-1; i >= 0; i--){
+        cout << str[i];
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
