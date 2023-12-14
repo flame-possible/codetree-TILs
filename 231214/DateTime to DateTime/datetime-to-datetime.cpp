e6ebb8ce61b45b08351e72dbc6a2d514f3961f5e
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-12-14 19:31:30 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231214/DateTime to DateTime/datetime-to-datetime.cpp
+++ 231214/DateTime to DateTime/datetime-to-datetime.cpp
@@ -8,15 +8,17 @@ int main() {
 
     cin >> a >> b >> c;
 
-    if(a <= 11 && b <= 11 && c < 11){
-        cout << "-1\n";
-        return 0;
-    }
-
     int standard_sum = (11 * 1440) + (11 * 60) + 11;
     int sum = (a * 1440) + (b * 60) + c;
 
-    cout << sum - standard_sum << '\n';
+    int result = sum - standard_sum;
+
+    if(result < 0){
+        cout << "-1\n";
+    }
+    else{
+        cout << result << '\n';
+    }
 
     return 0;
 }
