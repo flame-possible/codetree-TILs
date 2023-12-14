363eba4e1cdcb0351859896cf99c4457044b566c
flame-possible <104985055+flame-possible@users.noreply.github.com>
2023-12-14 20:12:53 +0900

    Title: 그 요일은, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231214/그 요일은/the-day-of-the-day.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int mon[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+string str[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+int count_mon(int m, int d){
+    int sum = 0;
+
+    for(int i = 1; i < m; i++){
+        sum += mon[i];
+    }
+    sum += d;
+
+    return sum;
+}
+
+int main() {
+
+
+    int m1, d1, m2, d2;
+    string input;
+
+    cin >> m1 >> d1 >> m2 >> d2;
+    cin >> input;
+
+    int stan = count_mon(m1, d1);
+    int tar = count_mon(m2, d2);
+
+    int diff = count_mon(m1, d1) - count_mon(m2, d2);
+    int cnt = 0;
+    int pos = 0;
+
+    for(int i = 0; i < 7; i++){
+        if(str[i] == input){
+            pos = i;
+            break;
+        }
+    }
+
+    while(diff < 0){
+        diff += 7;
+        cnt++;
+    }
+
+    if(pos <= diff){
+        cnt++;
+    }
+
+    cout << cnt << '\n';
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
