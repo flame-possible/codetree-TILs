621c117817a8412fc92e283c4d4f54b40a52ac3b
flame-possible <104985055+flame-possible@users.noreply.github.com>
2024-01-04 20:58:32 +0900

    Title: 흰검 칠하기, Time: 8ms, Memory: 2MB, Status: Passed - CodeTree

--- 240104/흰검 칠하기/painting-white-black.cpp
+++ 240104/흰검 칠하기/painting-white-black.cpp
@@ -7,13 +7,13 @@ int main() {
     int N;
     int x;
     char pos;
-    int last[2001] = {0,};
-    int black[2001] = {0,};
-    int white[2001] = {0,};
+    int last[200001] = {0,};
+    int black[200001] = {0,};
+    int white[200001] = {0,};
 
     cin >> N;
 
-    int cur = 1000;
+    int cur = 100000;
     for(int i = 0; i < N; i++){
         cin >> x >> pos;
 
@@ -38,7 +38,7 @@ int main() {
     int black_num = 0;
     int white_num = 0;
 
-    for(int i = 0; i <= 2000; i++){
+    for(int i = 0; i <= 200000; i++){
         if(black[i] >= 2 && white[i] >= 2){
             gray_num++;
         }
