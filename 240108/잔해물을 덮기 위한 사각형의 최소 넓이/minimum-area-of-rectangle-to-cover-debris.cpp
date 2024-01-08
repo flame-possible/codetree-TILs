3071f5af802755c20bc6e68df47615d83d690fdf
flame-possible <104985055+flame-possible@users.noreply.github.com>
2024-01-08 20:56:46 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 18ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240108/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int x1, y1, x2, y2;
+    int input[2001][2001] = {0,};
+
+    for(int i = 0; i < 2; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        x1 += 1000;
+        y1 += 1000;
+        x2 += 1000;
+        y2 += 1000;
+
+        for(int k = x1; k < x2; k++){
+            for(int p = y1; p < y2; p++){
+                if(i == 0)
+                    input[k][p] = 1;
+                else
+                    input[k][p] = 0;
+            }
+        }
+
+    }
+
+    x1 = y1 = 2001;
+    x2 = y2 = 0;
+
+    int result = 0;
+    for(int i = 0; i < 2001; i++){
+        for(int k = 0; k < 2001; k++){
+            if(input[i][k]){
+                if(x1 > i){
+                    x1 = i;
+                }
+                if(x2 < i){
+                    x2 = i + 1;
+                }
+                if(y1 > k){
+                    y1 = k;
+                }
+                if(y2 < k){
+                    y2 = k + 1;
+                }
+            }
+        }
+    }
+
+    // cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << '\n';
+
+    result = (x2 - x1) * (y2 - y1);
+
+    cout << result << '\n';
+
+    return 0;
+}
