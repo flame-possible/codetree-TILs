aba2f57d3243f4a1b65f59c14dcb0d977a27eb97
flame-possible <104985055+flame-possible@users.noreply.github.com>
2024-01-08 19:52:37 +0900

    Title: 사각형의 총 넓이 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240108/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int N;
+    int input[201][201] = {0,};
+
+    int x1, x2, y1, y2;
+
+    cin >> N;
+
+    for(int i = 0; i < N; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        x1 += 100;
+        y1 += 100;
+        x2 += 100;
+        y2 += 100;
+
+        for(int k = x1; k < x2; k++){
+            for(int p = y1; p < y2; p++){
+                input[k][p] = 1;
+            }
+        }
+    }
+
+    int result = 0;
+
+    for(int i = 0; i < 201; i++){
+        for(int k = 0; k < 201; k++){
+            if(input[i][k]){
+                result++;
+            }
+        }
+    }
+
+    cout << result << '\n';
+
+    return 0;
+}
