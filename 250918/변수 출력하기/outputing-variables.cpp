65b2b2363b334d158513a3230bbc3feb3678a03f
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-18 19:46:19 +0900

    Title: 변수 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250918/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 3;
+    char c = 'C';
+
+    cout << a << "..." << c;
+
+    return 0;
+}
