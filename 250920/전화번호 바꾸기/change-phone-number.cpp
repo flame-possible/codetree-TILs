df568828734b77389f570433d9e3b08eb674e8fe
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 21:37:10 +0900

    Title: 전화번호 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, x, y;
+    char t;
+
+    cin >> a >> t >> x >> t >> y;
+
+    cout << "010-" << y << '-' << x;
+
+    return 0;
+}
