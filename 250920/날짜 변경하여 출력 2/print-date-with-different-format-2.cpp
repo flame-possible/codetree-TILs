3f43742db3f945b59c8f6467d020e85b8f1459bd
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 21:33:32 +0900

    Title: 날짜 변경하여 출력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int m, d, y;
+    char a;
+
+    cin >> m >> a >> d >> a >> y;
+
+    cout << y << '.' << m << '.' << d;
+
+    return 0;
+}
