428daf22190052d944c51256a5b115e0e8ae8266
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 16:46:13 +0900

    Title: 소수점 반올림하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    double a = 25.352;
+
+    cout.precision(3);
+    cout << a;
+
+    return 0;
+}
