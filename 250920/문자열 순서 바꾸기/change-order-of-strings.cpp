db3349803cd42656776f68cdb1c023762b7455fb
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 18:18:11 +0900

    Title: 문자열 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/문자열 순서 바꾸기/change-order-of-strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string a, b;
+
+    cin >> a >> b;
+
+    cout << b << '\n' << a;
+
+    return 0;
+}
