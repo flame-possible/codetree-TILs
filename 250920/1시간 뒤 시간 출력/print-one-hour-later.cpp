8d48e028b329e3b58cb48030c2b44c1efaa0fcde
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 19:10:54 +0900

    Title: 1시간 뒤 시간 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int h, m;
+    char a;
+
+    cin >> h >> a >> m;
+
+    cout << h+1 << a << m;
+
+    return 0;
+}
