3ca3375c4525c124e711b05e4ce31beefa142778
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-20 17:04:20 +0900

    Title: 변수 값 교체하기 6, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250920/변수 값 교체하기 6/replacing-variable-values-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 2, b = 5;
+    
+    int temp = a;
+    a = b;
+    b = temp;
+
+    cout << a << '\n' << b;
+
+    return 0;
+}
