7567cd9ecd3cd2f62de2d353b5347ad1ff838033
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 19:43:04 +0900

    Title: 일 수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int result[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    int n;
+
+    cin >> n;
+
+    cout << result[n];
+
+    return 0;
+}
