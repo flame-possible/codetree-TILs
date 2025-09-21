8407e29ae53a507c8f7e1a774d9e1024b44b823c
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 19:38:33 +0900

    Title: 윤년인가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/윤년인가/is-leap-year.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int y;
+
+    cin >> y;
+
+    
+    if(y % 4 == 0){
+
+        if(y % 100 == 0){
+
+            if(y % 400 == 0){
+                cout << "true";
+            }
+            else{
+                cout << "false";
+            }
+
+        }
+        else{
+            cout << "true";
+        }
+        
+    }
+    else{
+        cout << "false";
+    }
+
+    return 0;
+}
