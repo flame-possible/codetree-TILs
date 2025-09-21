c7819da83e6f1110f328b3190478846276468fab
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 17:04:21 +0900

    Title: 살 수 있는 물건 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    if(N >= 3000){
+        cout << "book";
+    }
+    else if(N >= 1000){
+        cout << "mask";
+    }
+    else if(N >= 500){
+        cout << "pen";
+    }
+    else{
+        cout << "no";
+    }
+
+    return 0;
+}
