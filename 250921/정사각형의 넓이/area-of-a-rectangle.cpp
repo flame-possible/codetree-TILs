cd7dca2b862a10c42210ede5f0732b16cc99d972
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 14:46:06 +0900

    Title: 정사각형의 넓이, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    cout << N * N << '\n';
+
+    if(N < 5){
+        cout << "tiny";
+    }
+
+    return 0;
+}
