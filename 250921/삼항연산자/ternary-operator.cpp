06b3569d2b6bcd06c977b1b596b4e460a3b56541
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 14:54:20 +0900

    Title: 삼항연산자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    cout << (n == 100 ? "pass" : "failure");
+
+    return 0;
+}
