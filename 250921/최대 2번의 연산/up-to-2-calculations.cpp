0be28b7e530f492bdf8a73bd17706789976e43de
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 17:07:08 +0900

    Title: 최대 2번의 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+
+    cin >> a;
+
+    if((a % 2) == 0){
+        a /= 2;
+    }
+
+    if(a & 1){
+        a++;
+        a /= 2;
+    }
+
+    cout << a;
+
+    return 0;
+}
