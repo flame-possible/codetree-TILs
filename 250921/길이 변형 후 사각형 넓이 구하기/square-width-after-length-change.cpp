d3c601e4c40768f6955df224efaf61e262491113
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 14:19:23 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+
+    cin >> a >> b;
+
+    a += 8;
+    b *= 3;
+
+    cout << a << '\n';
+    cout << b << '\n';
+    cout << a * b << '\n';
+
+    return 0;
+}
