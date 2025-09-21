9a24475a5d5197a38a628ca2f99e730fd3f2147f
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-21 17:03:10 +0900

    Title: 알파벳에 따른 평가, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250921/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    char a;
+
+    cin >> a;
+
+    if(a == 'S'){
+        cout << "Superior";
+    }
+    else if(a == 'A'){
+        cout << "Excellent";
+    }
+    else if(a == 'B'){
+        cout << "Good";
+    }
+    else if(a == 'C'){
+        cout << "Usually";
+    }
+    else if(a == 'D'){
+        cout << "Effort";
+    }
+    else{
+        cout << "Failure";
+    }
+
+    return 0;
+}
