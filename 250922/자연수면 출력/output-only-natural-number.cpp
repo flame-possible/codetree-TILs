ea5ad0492242286b3d4c889e3dc54e8e35b97656
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 16:04:54 +0900

    Title: 자연수면 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250922/자연수면 출력/output-only-natural-number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a, b;
+
+    cin >> a >> b;
+
+    if(a < 0){
+        cout << "0";
+    }
+    else{
+        for(int i = 0; i < b; i++){
+            cout << a;
+        }
+    }
+
+    return 0;
+}
