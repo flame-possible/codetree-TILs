6e9f8897d3ecd6a424001dda091f929528336956
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 20:03:56 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    while(1){
+        int temp = 0;
+
+        cin >> temp;
+
+        if(temp == 0){
+            break;
+        }
+
+        cout << temp << '\n';
+    }
+
+    return 0;
+}
