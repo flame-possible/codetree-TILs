f447abb221e71725cc91d02a3a1d6ccb8397ec06
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 16:22:30 +0900

    Title: 홀수만 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int input;
+
+    for(int i = 0; i < n; i++){
+        cin >> input;
+
+        if(input & 1 && input % 3 == 0){
+            cout << input << '\n';
+        }
+    }
+
+    return 0;
+}
