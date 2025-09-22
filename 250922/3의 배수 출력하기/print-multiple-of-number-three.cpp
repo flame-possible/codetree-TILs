9b94801eae2b429c5974cd3d0a9b991336afd992
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 15:44:51 +0900

    Title: 3의 배수 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int i = 3;
+
+    while(i <= N){
+        cout << i << ' ';
+        i += 3;
+    }
+
+    return 0;
+}
