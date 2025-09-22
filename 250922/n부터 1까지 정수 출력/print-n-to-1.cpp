64af60dfd6b8f2350f49bf79abfa70e5d9cb1822
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 15:47:43 +0900

    Title: n부터 1까지 정수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/n부터 1까지 정수 출력/print-n-to-1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int i = n;
+
+    while(i >= 1){
+        cout << i << ' ';
+        i--;
+    }
+
+    return 0;
+}
