d3051e91d8b28c3977e2fbaa6b50284f5936c715
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 15:42:55 +0900

    Title: 별 N개 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int i = 1;
+
+    while(i <= N){
+        cout << "*\n";
+        i++;
+    }
+
+    return 0;
+}
