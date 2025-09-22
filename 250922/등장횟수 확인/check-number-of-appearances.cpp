1c660986e963ad696e2da0c92f155646a752a105
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 19:23:41 +0900

    Title: 등장횟수 확인, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/등장횟수 확인/check-number-of-appearances.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a;
+
+    int cnt = 0;
+
+    for(int i = 0; i < 5; i++){
+        cin >> a;
+
+        if(a % 2 == 0){
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
