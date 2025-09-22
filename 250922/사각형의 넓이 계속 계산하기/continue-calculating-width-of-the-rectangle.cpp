cde137c58ddaca571064e6ac976584d63ef55fd6
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 20:11:34 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    while(1){
+
+        int a, b;
+        char c;
+
+        cin >> a >> b >> c;
+
+        cout << a * b << '\n';
+
+        if(c == 'C'){
+            break;
+        }
+
+
+    }
+
+    return 0;
+}
