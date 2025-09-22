3f4ed87e2e0a2446e2532eee6b72b02355aab6aa
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 15:42:00 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int i = 1;
+
+    while(i <= N){
+        cout << i << ' ';
+        i++;
+    }
+
+    return 0;
+}
