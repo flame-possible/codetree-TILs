d8a572df92d723d86e591a946c6ba6eb1172702e
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-22 15:35:52 +0900

    Title: 특정 숫자 도달하기 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250922/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    for(int i = N; i >= 1; i--){
+        cout << i << ' ';
+    }
+
+    return 0;
+}
