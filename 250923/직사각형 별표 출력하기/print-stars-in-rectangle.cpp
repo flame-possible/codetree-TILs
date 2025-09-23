29f770fcb9a4501e1c15139c5c44eb210a91e3f9
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-23 14:58:17 +0900

    Title: 직사각형 별표 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250923/직사각형 별표 출력하기/print-stars-in-rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n, m;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < m; k++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
