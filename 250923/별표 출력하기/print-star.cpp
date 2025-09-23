32638f85f38b3c93c86848a4b019fb635f25b6e3
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-23 23:20:44 +0900

    Title: 별표 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250923/별표 출력하기/print-star.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < i + 1; k++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+
+    for(int i = n - 1; i > 0; i--){
+        for(int k = 0; k < i; k++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
