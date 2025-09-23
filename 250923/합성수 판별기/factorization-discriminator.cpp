c819304c3551cd3f9fa56d3d631aea9aac0d57c4
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-23 14:47:03 +0900

    Title: 합성수 판별기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250923/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int result = 0;
+
+    for(int i = 2; i < n; i++){
+        if(n % i == 0){
+            result = 1;
+            break;
+        }
+    }
+
+    if(result & 1){
+        cout << "C";
+    }
+    else{
+        cout << "N";
+    }
+
+    return 0;
+}
