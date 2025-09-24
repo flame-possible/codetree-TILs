aab52f8f5a4f2e44f5f2c2f572bb4631296bdc8c
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 01:37:26 +0900

    Title: 열 단위로 반복하여 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/열 단위로 반복하여 출력/print-in-column.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int k = 0; k < n; k++){
+            cout << i;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
