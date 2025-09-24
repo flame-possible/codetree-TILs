11ac39d4c77dc881ea09bdde8736712b0250986d
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 01:42:06 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = n; i > 0; i--){
+        for(int k = n; k > 0; k--){
+            cout << k << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
