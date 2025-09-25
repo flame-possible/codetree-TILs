409ed19e924b1533d3d3ea10e9461e5d21f5d493
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 12:30:09 +0900

    Title: ab곱 N번 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/ab곱 N번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        int a, b;
+
+        cin >> a >> b;
+
+        int result = 1;
+
+        for(int k = a; k <= b; k++){
+            result *= k;
+        }
+
+        cout << result << '\n';
+    }
+
+    return 0;
+}
