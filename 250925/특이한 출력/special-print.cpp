387c6948d624ccfd394e50afd412459ef1a042a2
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 12:16:45 +0900

    Title: 특이한 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/특이한 출력/special-print.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n; k++){
+            cout << "(" << i << ", " << k << ")" << ' ';
+
+            if((i + k) % 4 == 0){
+                cout << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
