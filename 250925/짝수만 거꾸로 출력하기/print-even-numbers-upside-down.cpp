028599e9021a3fa37e617e9ddea988a1ae3bfa2d
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 16:44:15 +0900

    Title: 짝수만 거꾸로 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/짝수만 거꾸로 출력하기/print-even-numbers-upside-down.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int input[n] = {0,};
+    int output[n] = {0,};
+
+    int cnt = 0;
+
+    for(int i = 0; i < n; i++){
+        cin >> input[i];
+
+        if(input[i] % 2 == 0){
+            output[cnt++] = input[i];
+        }
+    }
+
+    for(int i = cnt - 1; i >= 0; i--){
+        cout << output[i] << ' ';
+    }
+
+    return 0;
+}
