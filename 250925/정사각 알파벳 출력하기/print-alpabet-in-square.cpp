c0a2c98d9e5dfa2fc40ab6b2f4f303c146cf97ae
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-25 12:23:25 +0900

    Title: 정사각 알파벳 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250925/정사각 알파벳 출력하기/print-alpabet-in-square.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    char cnt = 'A';
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < n; k++){
+            cout << cnt++;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
