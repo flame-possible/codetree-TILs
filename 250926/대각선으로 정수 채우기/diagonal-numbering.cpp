68d952c9192f43738335c959e225793adf36386e
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-26 16:40:25 +0900

    Title: 대각선으로 정수 채우기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250926/대각선으로 정수 채우기/diagonal-numbering.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n, m;
+
+    cin >> n >> m;
+
+    int input[n][m] = {0,};
+
+    int cury = 0, curx = 0;
+
+    int cnt = 1;
+
+    while(cury != n - 1 || curx != m - 1){
+
+        for(int i = cury, k = curx; i < n && k >= 0; i++, k--){
+            input[i][k] = cnt++;
+        }
+
+        curx++;
+
+        if(curx >= m){
+            curx--;
+            cury++;
+        }
+
+        if(cury == n - 1 && curx == m - 1){
+            input[cury][curx] = cnt;
+            break;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < m; k++){
+            cout << input[i][k] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
