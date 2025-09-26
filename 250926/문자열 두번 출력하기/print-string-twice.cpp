065ae25ccc4aa9b939e3880ad83114ca66654763
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-26 18:03:56 +0900

    Title: 문자열 두번 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string input;
+
+    cin >> input;
+
+    cout << input << '\n' << input;
+
+    return 0;
+}
