4ecd2104757f1cfe2b8c732e05eb7ad3cdcb5f90
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-26 15:02:16 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/가장 왼쪽에 있는 최댓값/leftmost-max-value.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int N;
+int a[1001];
+
+int main() {
+    cin >> N;
+
+    int max = INT_MIN;
+    int cnt = N;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> a[i];
+    }
+
+    while(cnt != 0){
+
+        for(int i = 1; i <= cnt; i++){
+            if(a[i] > max){
+                max = a[i];
+            }
+        }
+
+        for(int i = 1; i <= cnt; i++){
+            if(a[i] == max){
+                cnt = i;
+            }
+        }
+
+        cout << cnt-- << ' ';
+
+        max = 0;
+
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
