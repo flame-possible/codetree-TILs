e976d6d8041fba0322d272e1dfe7719ccdcc8f04
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-26 18:02:48 +0900

    Title: 격자 모양 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/격자 모양 출력하기/print-grid-shape.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n, m;
+
+    cin >> n >> m;
+
+    int input[n+1][n+1] = {0,};
+
+    for(int i = 0; i < m; i++){
+        int y, x;
+
+        cin >> y >> x;
+
+        input[y][x] = y * x;
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n; k++){
+            cout << input[i][k] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
