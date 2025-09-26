c5cca2c663a58f58135e9573e1034a12ca6bd175
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-26 18:00:49 +0900

    Title: 격자에 점 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250926/격자에 점 그리기/draw-points-on-grid.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n, m;
+
+    cin >> n >> m;
+
+    int input[n+1][n+1] = {0,};
+
+    int cnt = 1;
+
+    for(int i = 0; i < m; i++){
+        int y, x;
+
+        cin >> y >> x;
+
+        input[y][x] = cnt++;
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n; k++){
+            cout << input[i][k] << ' ';
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
