16fe6ad1e0313c23e1bc55737039beda4cd12bec
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-28 16:26:55 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250928/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string input[10];
+
+    for(int i = 0; i < 10; i++){
+        cin >> input[i];
+
+        cout << input[i] << '\n';
+    }
+
+    
+
+    return 0;
+}
