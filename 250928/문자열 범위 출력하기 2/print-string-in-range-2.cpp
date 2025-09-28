adebc2b298411d2564f3b86a47b71105d8b93825
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-28 16:43:27 +0900

    Title: 문자열 범위 출력하기 2, Time: 21ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 250928/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 250928/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -14,7 +14,9 @@ int main() {
     cin >> cnt;
 
     if(cnt > input.length()){
-        cout << input;
+        for(int i = input.length() - 1; i >= 0; i--){
+            cout << input[i];
+        }
     }
     else{
         for(int i = input.length() - 1; i >= input.length() - cnt; i--){
