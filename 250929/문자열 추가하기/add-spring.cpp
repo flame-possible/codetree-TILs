5765dc194cbddbb64172eb0e9936270e6b701cc1
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-29 15:43:18 +0900

    Title: 문자열 추가하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250929/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string input;
+
+    cin >> input;
+
+    input += "Hello";
+
+    cout << input;
+
+    return 0;
+}
