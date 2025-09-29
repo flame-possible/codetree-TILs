92737084201403a5d6b5bfa58e8409b22391fd7b
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-29 18:13:39 +0900

    Title: 문자 교체하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250929/문자 교체하기/changing-char.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string a, b;
+
+    cin >> a >> b;
+
+    b[0] = a[0];
+    b[1] = a[1];
+
+    cout << b;
+
+    return 0;
+}
