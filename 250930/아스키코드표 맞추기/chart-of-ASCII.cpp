b55c7b055e8dccf503b2a3c3e404bf1063fafcf0
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-09-30 17:40:38 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250930/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    for(int i = 0; i < 5; i++){
+        int input;
+
+        cin >> input;
+
+        cout << (char) input << ' ';
+    }
+
+    return 0;
+}
