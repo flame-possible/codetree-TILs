a4b35a1ae9f568757a14c392657a2f2efeb6f36f
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-01 00:35:16 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251001/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string a, b;
+
+    cin >> a >> b;
+
+    cout << stoi(a) + stoi(b);
+
+    return 0;
+}
