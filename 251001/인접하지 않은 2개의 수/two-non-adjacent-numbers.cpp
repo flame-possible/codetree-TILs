be6b8af10de72eb9d187743bcaeeaf9379e09884
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-01 00:58:38 +0900

    Title: 인접하지 않은 2개의 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251001/인접하지 않은 2개의 수/two-non-adjacent-numbers.cpp
+++ 251001/인접하지 않은 2개의 수/two-non-adjacent-numbers.cpp
@@ -15,11 +15,7 @@ int main() {
     int result = INT_MIN;
 
     for(int i = 0; i < n; i++){
-        for(int k = 0; k < n; k++){
-            if(i == k || i == k - 1 || i == k + 1){
-                continue;
-            }
-
+        for(int k = i + 2; k < n; k++){
             if(numbers[i] + numbers[k] > result){
                 result = numbers[i] + numbers[k];
             }
