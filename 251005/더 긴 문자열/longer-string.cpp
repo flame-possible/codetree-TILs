97bd153cd71e261e212c3f1b1a5bbc65884c4e9a
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-05 21:21:23 +0900

    Title: 더 긴 문자열, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string a, b;
+
+    cin >> a >> b;
+
+    if(a.size() > b.size()){
+        cout << a << ' ' << a.size();
+    }
+    else if(a.size() < b.size()){
+        cout << b << ' ' << b.size();
+    }
+    else{
+        cout << "same";
+    }
+
+    return 0;
+}
