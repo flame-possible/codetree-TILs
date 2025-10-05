750e8badcc5b368bcef37b33327ccc5361427ba4
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-05 17:50:07 +0900

    Title: 세 정수형 변수 선언, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a = 7, b = 23;
+
+    cout << a << " + " << b << " = " << a + b;
+
+    return 0;
+}
