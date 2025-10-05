3dc53c827072794e320ff27071b9db02f7decc0d
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-05 18:52:59 +0900

    Title: 제곱하여 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int a[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+
+        cout << a[i] * a[i] << ' ';
+    }
+
+    return 0;
+}
