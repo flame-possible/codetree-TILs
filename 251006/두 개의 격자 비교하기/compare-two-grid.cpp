68267a03808718fe2cfe7a5d38a49d8d1385d185
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-06 14:30:10 +0900

    Title: 두 개의 격자 비교하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n, m;
+
+    cin >> n >> m;
+
+    int input1[n][m];
+    int input2[n][m];
+
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < m; k++){
+            cin >> input1[i][k];
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < m; k++){
+            cin >> input2[i][k];
+        }
+    }
+
+    
+    for(int i = 0; i < n; i++){
+        for(int k = 0; k < m; k++){
+            if(input1[i][k] == input2[i][k]){
+                cout << "0 ";
+            }
+            else{
+                cout << "1 ";
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
