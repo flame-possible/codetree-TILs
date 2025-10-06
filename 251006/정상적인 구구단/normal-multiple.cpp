c6e484162823e92c2822b6e3764d1060b893d7cd
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-06 14:14:25 +0900

    Title: 정상적인 구구단, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int k = 1; k <= n; k++){
+            cout << i << " * " << k << " = " << i * k;
+
+            if(k != n){
+                cout << ", ";
+            }
+        }
+
+        cout << '\n';
+    }
+
+    return 0;
+}
