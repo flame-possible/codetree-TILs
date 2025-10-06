2b83ebe23ad11cd3694caa2f2c8eec7922e46000
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-06 09:28:44 +0900

    Title: 1부터의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251006/1부터의 합/sum-from-the-one.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+
+    cin >> n;
+
+    int result = 0;
+
+    for(int i = 1; i <= 100; i++){
+        result += i;
+
+        if(result >= n){
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
