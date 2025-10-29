4fbf64d175b058c1a8200e8217c3da3d40ca8cd3
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-10-29 23:25:45 +0900

    Title: 상해버린 치즈, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251029/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Input{
+    vector<pair<int, int>> info;
+};
+
+int m_info[51];
+int p_info[51];
+
+int main() {
+    // Please write your code here.
+
+    int N, M, D, S;
+
+    cin >> N >> M >> D >> S;
+
+    Input input[101];
+
+
+    for(int i = 0; i < D; i++){
+        int p, m, t;
+
+        cin >> p >> m >> t;
+
+        input[t].info.push_back({p, m});
+    }
+
+    for(int i = 0; i < S; i++){
+        int m, t;
+
+        cin >> m >> t;
+
+        for(int k = 1; k < t; k++){
+            for(int p = 0; p < input[k].info.size(); p++){
+                if(input[k].info[p].first == m){
+                    m_info[m] = 1;
+                }
+            }
+        }
+    }
+
+    int result = 0;
+
+    for(int i = 1; i <= M; i++){
+        if(m_info[i]){
+            for(int k = 1; k <= 100; k++){
+                for(int p = 0; p < input[k].info.size(); p++){
+                    if(input[k].info[p].second == i){
+                        p_info[input[k].info[p].first] = 1;
+                    }
+                }
+            }
+        }
+    }
+
+    for(int i = 1; i <= N; i++){
+        result += p_info[i];
+    }
+
+    cout << result;
+
+    return 0;
+}
