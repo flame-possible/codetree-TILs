1b1a39ac4884a94d356349f8967f33e28e9a2c74
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-05 16:03:36 +0900

    Title: 데이터센터의 온도 조정 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251105/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int N, C, G, H;
+int T[1001][2];
+
+int main() {
+    // Please write your code here.
+
+    cin >> N >> C >> G >> H;
+
+    int TMin = 1000;
+    int TMax = 0;
+
+    for(int i = 0; i < N; i++){
+        cin >> T[i][0] >> T[i][1];
+
+        TMin = min(TMin, T[i][0]);
+        TMax = max(TMax, T[i][1]);
+    }
+
+    int result = 0;
+
+    for(int i = TMin; i <= TMax; i++){
+        int temp = 0;
+
+        for(int k = 0; k < N; k++){
+            if(i < T[k][0]){
+                temp += C;
+            }
+            else if(i >= T[k][0] && i <= T[k][1]){
+                temp += G;
+            }
+            else{
+                temp += H;
+            }
+        }
+
+        result = max(result, temp);
+
+    }
+
+    cout << result;
+
+
+
+    return 0;
+}
