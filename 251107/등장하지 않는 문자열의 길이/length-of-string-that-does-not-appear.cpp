f09aa921bad44fef1495e47ce7972ea853daa23f
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-07 19:16:25 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251107/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    string input;
+
+    cin >> input;
+
+    for(int i = 1; i <= N; i++){
+        
+        if(i > N - i){
+            cout << i;
+            break;
+        }
+
+        int temp = 0;
+
+        for(int k = 0; k <= N - (i * 2); k++){
+            string dns = input.substr(k, i);
+
+            temp = 0;
+
+            for(int p = k + i; p <= N - i; p++){
+                string tar = input.substr(p, i);
+
+                if(dns == tar){
+                    temp = 1;
+
+                    break;
+                }
+            }
+
+            if(temp) break;
+        }
+
+        if(!temp){
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
