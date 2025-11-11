4dc06e7d6b1a29977e9fdf4c2b301fa9b1f6dc29
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-11 14:37:43 +0900

    Title: 수들의 최대 차, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251111/수들의 최대 차/maximum-difference-in-numbers.cpp
+++ 251111/수들의 최대 차/maximum-difference-in-numbers.cpp
@@ -1,44 +1,9 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int N, K;
 int input[1001];
-int temp[1001];
-
-int answer(int cur, int cnt, int tar){
-    if(cnt == tar){
-        int temp_max = 0;
-        int temp_min = 10001;
-
-        for(int i = 0; i < cnt; i++){
-            temp_max = max(temp_max, temp[i]);
-            temp_min = min(temp_min, temp[i]);
-        }
-
-        if((temp_max - temp_min) <= K){
-            return 1;
-        }
-        else{
-            return 0;
-        }
-    }
-
-    int result = 0;
-
-    for(int i = cur; i < N; i++){
-        temp[cnt++] = input[i];
-
-        result = answer(i + 1, cnt, tar);
-
-        if(result) return 1;
-
-        cnt--;
-
-    }
-
-    return 0;
-
-}
 
 int main() {
     // Please write your code here.
@@ -49,8 +14,18 @@ int main() {
         cin >> input[i];
     }
 
+    sort(input, input + N);
+
     for(int i = N; i >= 1; i--){
-        if(answer(0, 0, i)){
+        int result = 0;
+        for(int k = 0; k < N - i + 1; k++){
+            if((input[k+i-1] - input[k]) <= K){
+                result = 1;
+                break;
+            }
+        }
+
+        if(result){
             cout << i;
             break;
         }
