d8e92654a966d219eae2f085399476255a2880cd
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-13 14:56:47 +0900

    Title: 훌륭한 점프, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251113/훌륭한 점프/great-jump.cpp
+++ 251113/훌륭한 점프/great-jump.cpp
@@ -7,17 +7,15 @@ int input[101];
 int answer(int tar){
 
     int temp[101] = {0,};
-    int tempCnt = 0;
+    int tempCnt = 1;
 
-    for(int i = 0; i < N; i++){
+    for(int i = 1; i < N-1; i++){
         if(input[i] <= tar){
             temp[tempCnt++] = i;
         }
     }
 
-    if(tempCnt <= 1){
-        return 0;
-    }
+    temp[tempCnt++] = N-1;
 
     for(int i = 0; i < tempCnt - 1; i++){
         if(K < abs(temp[i] - temp[i+1])){
@@ -40,7 +38,7 @@ int main() {
 
     int result = 0;
 
-    for(int i = 100; i >= 1; i--){
+    for(int i = 100; i >= max(input[0], input[N-1]); i--){
         if(answer(i)){
             result = i;
         }
