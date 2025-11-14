8e9b9ee313e276ddc38c3dc243e79c4fd8bd0f55
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-14 16:55:15 +0900

    Title: 초기 수열 복원하기, Time: 17ms, Memory: 0MB, Status: Passed - Codetree

--- 251114/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 251114/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -4,40 +4,37 @@ using namespace std;
 int N;
 int input[1001];
 
-int pos[1001][1001];
-int candidate[1001][1001];
-int candidateCnt[1001];
 int target[1001];
 int check[1001];
 
 int answer(int cnt){
 
-    if(cnt == N){
+    if(cnt > 1){
         
-        int result = 1;
+        if((target[cnt-2] + target[cnt-1]) == input[cnt - 2]){
 
-        for(int i = 0; i < cnt - 1; i++){
-            if(input[i] != target[i] + target[i+1]){
-                result = 0;
-                break;
+            if(cnt == N){
+                return 1;
             }
+            
         }
-
-        return result;
+        else{
+            return 0;
+        }
 
     }
 
-    for(int i = 0; i < candidateCnt[cnt]; i++){
-        if(!check[candidate[cnt][i]]){
-            check[candidate[cnt][i]] = 1;
+    for(int i = 1; i <= N; i++){
+        if(!check[i]){
+            check[i] = 1;
 
-            target[cnt] = candidate[cnt][i];
+            target[cnt] = i;
 
             if(answer(cnt+1)){
                 return 1;
             }
 
-            check[candidate[cnt][i]] = 0;
+            check[i] = 0;
         }
     }
 
@@ -53,36 +50,6 @@ int main() {
 
     for(int i = 0; i < N - 1; i++){
         cin >> input[i];
-    }
-
-    for(int i = 0; i < N - 1; i++){
-        for(int k = 1; k <= N; k++){
-            if(k < input[i] && abs(k - input[i]) <= N && k != abs(k - input[i])){
-                pos[i][k]++;
-                pos[i+1][abs(k - input[i])]++;
-            }
-        }
-    }
-
-    int maxcnt[N] = {0,};
-
-    for(int i = 0; i < N; i++){
-        for(int k = 1; k <= N; k++){
-            maxcnt[i] = max(maxcnt[i], pos[i][k]);
-        }
-    }
-
-    for(int i = 0; i < N; i++){
-        for(int k = 1; k <= N; k++){
-            if(maxcnt[i] == pos[i][k]){
-                candidate[i][candidateCnt[i]++] = k;
-            }
-        }
-
-        // for(int k = 0; k < candidateCnt[i]; k++){
-        //     cout << candidate[i][k] << ' ';
-        // }
-        // cout << '\n';
     }
 
     answer(0);
