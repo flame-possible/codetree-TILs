6fd60908b26fd275065efd9ee93d606ce3d0ddc3
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-15 18:59:38 +0900

    Title: 언덕 깎기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251115/언덕 깎기/hill-cutting.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int input[101] = {0,};
+
+    for(int i = 0; i < N; i++){
+        int temp;
+
+        cin >> temp;
+
+        input[temp]++;
+    }
+
+    int result = INT_MAX;
+
+    for(int i = 0; i <= 100 - 17; i++){
+        int k = i + 17;
+
+        int temp = 0;
+
+        for(int p = 0; p < i; p++){
+            temp += input[p] * (i - p) * (i - p);
+        }
+
+        for(int p = 100; p > k; p--){
+            temp += input[p] * (p - k) * (p - k);
+        }
+
+        if(temp){
+            // cout << temp << '\n';
+            result = min(result, temp);
+        }
+
+        
+    }
+
+    cout << result;
+
+    return 0;
+}
