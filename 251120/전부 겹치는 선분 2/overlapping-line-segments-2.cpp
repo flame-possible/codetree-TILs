e094abaddafccbab4dc152943fc6de288d8888e9
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-20 00:21:48 +0900

    Title: 전부 겹치는 선분 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251120/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int input[101] = {0,};
+
+    for(int i = 0; i < N; i++){
+        int x1, x2;
+        cin >> x1 >> x2;
+
+        for(int i = x1; i <= x2; i++){
+            input[i]++;
+
+            if(input[i] >= N-1){
+                cout << "Yes";
+                return 0;
+            }
+        }
+
+    }
+
+    cout << "No";
+
+    return 0;
+}
