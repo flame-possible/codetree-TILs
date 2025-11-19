abbbf9cb4a1b1f4271b8f95ad6e0036d9c84ee09
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-19 23:58:32 +0900

    Title: 두 직사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251119/두 직사각형/two-rectangles.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int x1, y1, x2, y2;
+    int a1, b1, a2, b2;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> a1 >> b1 >> a2 >> b2;
+
+    if((x2 < a1) || (a2 < x1) || (y1 > b2) || (b1 > y2)){
+        cout << "nonoverlapping";
+    }
+    else{
+        cout << "overlapping";
+    }
+
+    return 0;
+}
