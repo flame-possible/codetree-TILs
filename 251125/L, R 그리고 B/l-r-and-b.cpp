241e83ec2d34e2a4a671d224c7e1b61f3932f738
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-25 16:08:59 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251125/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+char input[10][10];
+
+int main() {
+    // Please write your code here.
+
+    int L[2], R[2], B[2];
+
+    for(int i = 0; i < 10; i++){
+        for(int k = 0; k < 10; k++){
+            cin >> input[i][k];
+
+            if(input[i][k] == 'B'){
+                B[0] = i;
+                B[1] = k;
+            }
+            else if(input[i][k] == 'R'){
+                R[0] = i;
+                R[1] = k;
+            }
+            else if(input[i][k] == 'L'){
+                L[0] = i;
+                L[1] = k;
+            }
+        }
+    }
+
+    int result = 0;
+
+    if((B[0] == R[0] && R[0] == L[0]) || (B[1] == R[1] && R[1] == L[1])){
+        result += 2;
+    }
+
+    result += abs(B[0] - L[0]) + abs(B[1] - L[1]) - 1;
+
+    cout << result;
+
+    return 0;
+}
