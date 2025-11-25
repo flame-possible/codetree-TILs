af7a2546ef222f530e9f820ae8dffc7b531e3b25
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-25 13:51:22 +0900

    Title: 연속된 숫자 만들기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251125/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
+++ 251125/연속된 숫자 만들기 3/create-consecutive-numbers-3.cpp
@@ -9,30 +9,9 @@ int main() {
 
     cin >> input[0] >> input[1] >> input[2];
 
-    sort(input, input+3);
+    int result = max((input[1] - input[0]), (input[2] - input[1]));
 
-    int result = 0;
-
-    while(input[0] + 1 != input[1] || input[1] + 1 != input[2]){
-
-        if(input[1] - input[0] > input[2] - input[1]){
-            input[2] = input[1] - 1;
-        }
-        else{
-            input[0] = input[2] - 1;
-        }
-
-        result++;
-
-        sort(input, input+3);
-
-    }
-
-    // for(int i = 0; i < 3; i++){
-    //     cout << input[i] << ' ';
-    // }
-
-    cout << result;
+    cout << result - 1;
 
     return 0;
 }
