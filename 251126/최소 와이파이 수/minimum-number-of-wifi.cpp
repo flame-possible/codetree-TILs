cc3fa4a1ab75d7cf7a1e16c90c5ee5bfedea1d68
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-26 14:36:09 +0900

    Title: 최소 와이파이 수, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251126/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N, M;
+
+    cin >> N >> M;
+
+    int input[N];
+
+    for(int i = 0; i < N; i++){
+        cin >> input[i];
+    }
+
+    int checked[N] = {0,};
+
+    int result = 0;
+
+    for(int i = 0; i < N; i++){
+        if(!checked[i] && input[i]){
+            result++;
+            for(int k = i; k <= i + M + M; k++){
+                checked[k] = 1;
+            }
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
