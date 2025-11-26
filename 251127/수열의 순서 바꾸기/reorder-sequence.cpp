ee8fbc0e03f9ff69ad56660e4a5c365184e2f3cf
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-27 00:40:29 +0900

    Title: 수열의 순서 바꾸기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int input[101];
+int answer;
+
+void ans(int cnt){
+    int temp[101] = {0,};
+
+    int target = input[1];
+
+    for(int i = 2; i <= cnt; i++){
+        temp[i-1] = input[i];
+    }
+
+    for(int i = 1; i < cnt; i++){
+        input[i] = temp[i];
+    }
+
+    input[cnt] = target;
+    answer++;
+}
+
+int main() {
+    // Please write your code here.
+
+    cin >> N;
+
+    for(int i = 1; i <= N; i++){
+        cin >> input[i];
+    }
+
+    int result = 1;
+
+    for(int i = N; i > 1; i--){
+        if(input[i] > input[i-1]){
+            result++;
+        }
+        else{
+            break;
+        }
+    }
+
+    cout << N - result;
+
+    // for(int i = 1; i <= N; i++){
+    //     if(input[1] == 1){
+    //         for(int k = N; k > 1; k--){
+    //             if(input[k] < input[k-1]){
+    //                 ans(k-1);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     else{
+    //         for(int k = N; k > 1; k--){
+    //             if(input[k] < input[1]){
+    //                 ans(k);
+    //                 break;
+    //             }
+    //         }
+    //     }
+
+    //     int result = 1;
+
+    //     for(int k = 1; k <= N; k++){
+    //         cout << input[k] << ' ';
+    //         if(input[k] != k){
+    //             result = 0;
+    //         }
+    //     }
+    //     cout << '\n';
+
+    //     if(result){
+    //         cout << answer;
+    //         break;
+    //     }
+    // }
+
+    return 0;
+}
