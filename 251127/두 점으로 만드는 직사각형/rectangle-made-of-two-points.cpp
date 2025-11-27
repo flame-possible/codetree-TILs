a0b427d433464bbf832e890fef6d6e665eb86a87
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-11-27 15:48:18 +0900

    Title: 두 점으로 만드는 직사각형, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251127/두 점으로 만드는 직사각형/rectangle-made-of-two-points.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int x1, y1, x2, y2, a1, b1, a2, b2;
+
+    cin >> x1 >> y1 >> x2 >> y2 >> a1 >> b1 >> a2 >> b2;
+
+    int xMin = 11;
+    int xMax = 0;
+    int yMin = 11;
+    int yMax = 0;
+
+    xMin = min(xMin, min(x1, min(x2, min(a1, a2))));
+    xMax = max(xMax, max(x1, max(x2, max(a1, a2))));
+    
+    yMin = min(yMin, min(y1, min(y2, min(b1, b2))));
+    yMax = max(yMax, max(y1, max(y2, max(b1, b2))));
+
+    cout << (xMax - xMin) * (yMax - yMin);
+
+    return 0;
+}
