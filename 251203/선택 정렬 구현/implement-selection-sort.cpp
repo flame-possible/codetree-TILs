0f554509772fa6a106be364a54c8618cfa11e6c9
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-12-03 17:48:27 +0900

    Title: 선택 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251203/선택 정렬 구현/implement-selection-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int N;
+
+    cin >> N;
+
+    int input[N];
+
+    for(int i = 0; i < N; i++){
+        cin >> input[i];
+    }
+
+    for(int i = 0; i < N; i++){
+        int min = i;
+
+        for(int k = i; k < N; k++){
+            if(input[k] < input[min]){
+                min = k;
+            }
+        }
+
+        int temp = input[i];
+        input[i] = input[min];
+        input[min] = temp;
+    }
+
+    for(int i = 0; i < N; i++){
+        cout << input[i] << ' ';
+    }
+
+    return 0;
+}
