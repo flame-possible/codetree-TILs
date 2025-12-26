919830cce8fd73cc44a59ef3ace6388daf6136a5
flame-possible <104985055+flame-possible@users.noreply.github.com>
2025-12-26 13:28:43 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 17ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251226/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    string input;
+
+    cin >> input;
+
+    stack<char> s;
+
+    for(int i = 0; i < input.size(); i++){
+        if(input[i] == '('){
+            s.push('(');
+        }
+        else{
+            if(s.top() == '('){
+                s.pop();
+            }
+            else{
+                cout << "No" << '\n';
+                return 0;
+            }
+        }
+    }
+
+    if(s.size() != 0){
+        cout << "No" << '\n';
+    }
+    else{
+        cout << "Yes" << '\n';
+    }
+
+    return 0;
+}
