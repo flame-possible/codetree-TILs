c7f68679a50b6dcc6f8cbe6d2465aaa4766c2fe5
flame-possible <104985055+flame-possible@users.noreply.github.com>
2026-02-10 16:34:57 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260210/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+
+int N, M;
+
+int main() {
+    // Please write your code here.
+
+    cin >> N >> M;
+
+    int input[N][N];
+
+    for(int i = 0; i < N; i++){
+        for(int k = 0; k < N; k++){
+            cin >> input[i][k];
+        }
+    }
+
+    int result = 0;
+
+    for(int i = 0; i < N; i++){
+        int cur = input[i][0];
+        int cnt = 1;
+
+        for(int k = 1; k < N; k++){
+            
+            if(cur == input[i][k]){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+
+            if(cnt >= M){
+                result++;
+                break;
+            }
+        }
+    }
+
+    for(int i = 0; i < N; i++){
+        int cur = input[0][i];
+        int cnt = 1;
+
+        for(int k = 1; k < N; k++){
+            
+            if(cur == input[k][i]){
+                cnt++;
+            }
+            else{
+                cnt = 1;
+            }
+
+            if(cnt >= M){
+                result++;
+                break;
+            }
+        }
+    }
+
+    cout << result;
+
+    return 0;
+}
